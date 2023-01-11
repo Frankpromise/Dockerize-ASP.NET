bf7bf44051e13803c5277bae40e21af5aa84360e
promise <promise@DESKTOP-5V7NG46.localdomain>
2023-01-11 10:03:15 +0100

    folder for mvc version of .net

---
+++ ASP/samplewithdocker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace samplewithdocker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
